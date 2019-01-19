9300bb8894d3d09a47b3cf8b52f63fba2a841d8e
Ivan <ivanleomk@gmail.com>
2019-01-19 21:24:00 +0800

    First Commit

---
+++ journal/dashboard/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import datetime
+
+class entry(models.Model):
+    entry = models.CharField(max_length=200)
+    entry_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.entry
